5fb735a1f7391cc92ddc626dc9bdb42e947ed467
Bilal Saeed <bilalsaeedhb@gmail.com>
2019-11-22 19:31:56 +0500

    Initialized boilerplate

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+import '../styles/index.scss';
+
+const output = document.getElementById("main");
+let stream = '';
+
+function appendOutput(string) {
+  stream = `${stream}${'<br />'}${string}`;
+}
+
+( () => {
+  const string = "Here's the output: ";
+  appendOutput(string);
+
+  output.innerHTML = stream;
+})();
+
