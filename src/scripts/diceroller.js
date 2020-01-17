0af3e99f25b066523ec4762fcdb3e2e8917b210d
AhmedKhan64 <59909325+AhmedKhan64@users.noreply.github.com>
2020-01-17 20:04:47 +0000

    Update diceroller.js
    is me split k function pr error dy ra hy k cant split the value of null

--- src/scripts/diceroller.js
+++ src/scripts/diceroller.js
@@ -1,1 +1,19 @@
 
+//to create random numbers
+let ran1 = Math.floor(Math.random() * 7);
+let ran2 = Math.floor(Math.random() * 7);
+//to trigger prompt and to split its values
+let data=prompt("Enter the guess for dice").split(",")
+//array to store splitted values of prompt
+let array= [ ]
+//to push prompt entries into array
+array.push(data.entries)
+//to compare prompt array with random numbers
+      let a=array[0]
+      let b=array[1]
+ //if then else to compare     
+    if (a==ran1,b==ran2) {alert("winner")
+        
+    } else {alert("try again")
+
+}
